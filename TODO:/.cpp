6b9f7d0f1cc7b6a4a33e11dc434158dce366dba0
danielediazp <98777117+danielediazp@users.noreply.github.com>
2022-07-02 12:16:01 -0400

    Create .cpp

---
+++ TODO:/.cpp
@@ -0,0 +1,21 @@
+//TODO:
+
+
+// 1. Implement duplicaates. Difficulty -> eassy 
+
+//       Steps:
+//         #Add a new data member to the node class 
+//         #Add a conditional to the insert method. If the node exist already, update it counter by one. 
+
+// 2. Implement deletion. Difficulty -> Hard 
+
+//   #Find the node. 
+//   #check to see if node has childs. If it is a leaf node, cut the node from the tree.
+//   #otherwise, some rotation will be needed. 
+//   #If Node is the root, find the predecessor or succesor. "Recomender DFS approach" 
+  
+  
+// 3. If you have any library in mind for graphcis rendering make a post on discord. 
+//   for now learn smfl and the following openGL library. 
+//  https://github.com/glium/glium
+  
