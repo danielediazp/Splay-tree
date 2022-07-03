6cd44dee06514ea43b14dea26cb718838f2109bc
danielediazp <98777117+danielediazp@users.noreply.github.com>
2022-07-03 14:41:42 -0400

    Update .cpp

--- TODO:/.cpp
+++ TODO:/.cpp
@@ -12,9 +12,14 @@
 //   #check to see if node has childs. If it is a leaf node, cut the node from the tree.
 //   #otherwise, some rotation will be needed. 
 //   #If Node is the root, find the predecessor or succesor. "Recomender DFS approach" 
-  
-  
-// 3. If you have any library in mind for graphcis rendering make a post on discord. 
+
+
+//Fix insert. Difficulty -? Med 
+//        #Add the splay method in each insert
+//        #every element insert in the tree should become the root of the tree 
+//        #refer to a youtube video for a deeper understanding 
+
+// 4. If you have any library in mind for graphcis rendering make a post on discord. 
 //   for now learn smfl and the following openGL library. 
 //  https://github.com/glium/glium
   
